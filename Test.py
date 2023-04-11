ccb11c81acd612be2150c918204fb9e3aba0cfb1
Maitret17 <114295382+Maitret17@users.noreply.github.com>
2023-04-11 15:23:37 +0200

    Add files via upload

--- Test.py
+++ Test.py
@@ -12,9 +12,7 @@ green = (0, 255, 0)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-width_window, height_window = 960, 640
-
-size = (width_window, height_window)
+size = (960, 640)
 window = pygame.display.set_mode(size)
 window.fill(white)
 
@@ -23,8 +21,8 @@ x_barre, y_barre = (960/2) - (width/2), 640 - height
 vel = 0.6 # La vitesse de la barre
 r=pygame.Rect(x_barre, y_barre, width, height)
 pygame.draw.rect(window, black, r)
-x_ball = 464
-y_ball = 288
+x_ball = 290
+y_ball = 190
 rayon = 20
 pygame.draw.ellipse(window, red, (x_ball,y_ball, rayon, rayon))
 vx, vy = random() * 0.6 - 0.3, .3
@@ -37,16 +35,16 @@ width_ball, height_ball = 32, 32
 pygame.display.update()
 
 
-def not_game_over(cpt, x_ball, y_ball, x_barre, y_barre, height, width, window): #Si le joue est toujours en cours
-    window.fill(white) # Pour empecher que les sprites d'avant restent
-    pygame.draw.rect(window, (black), (x_barre, y_barre, width, height)) # Redessine la barre avec ces nouvelles coordonnées
-    pygame.draw.ellipse(window, red, (int(x_ball), int(y_ball), width_ball, height_ball)) # Meme chose sur le cercle
-    image_texte = police.render(str(cpt), 1, (0, 0, 0)) # Affiche un le nombre de rebond sur la barre
+def not_game_over(cpt, x_ball, y_ball, x_barre, y_barre, height, width, window):
+    window.fill(white)
+    pygame.draw.rect(window, (black), (x_barre, y_barre, width, height))
+    pygame.draw.ellipse(window, red, (int(x_ball), int(y_ball), width_ball, height_ball))
+    image_texte = police.render(str(cpt), 1, (0, 0, 0))
     window.blit(police.render(str(cpt), 1, (0, 0, 0)), (10, 10))
     pygame.display.update()
     return image_texte
 
-def is_game_over(x_barre, y_barre, cpt, window): # Si game over
+def is_game_over(x_barre, y_barre, cpt, window):
     window.fill(white)
     police = pygame.font.SysFont("monospace", 80)
     image_texte = police.render ("GAME OVER", 1, (255, 0, 0))
@@ -65,30 +63,31 @@ while lock: # boucle pour maintenir la fenêtre ouverte
     
     for event in pygame.event.get():
         if event.type == QUIT:
-            lock = False # Si tu appuie sur la croix, cela ferme la fenetre
+            lock = False
         keys = pygame.key.get_pressed()
         
-    if keys[pygame.K_LEFT] and x_barre>0: # Si la touche fleche de gauche est appuyer, on retire la valeur vel (La vitesse de la barre) pour la faire aller à gauche. x_barre est la coordonnée x de la barre
+    if keys[pygame.K_LEFT] and x_barre>0:
         x_barre -= vel
         
-    if keys[pygame.K_RIGHT] and x_barre<width_window -width: # Meme chose pour la fleche de droite
+    if keys[pygame.K_RIGHT] and x_barre<960-width:
         x_barre += vel
         
-    if y_ball <= 32: # Si la balle touche le plafond
-        vy = -vy * 1.05 # la balle va s'inverser, elle change de sens.
-        vx *= (0.75+0.5*random()) # Pour plus de fun avec les rebonds
+    if y_ball <= 20:
+        vy = -vy * 1.05
+        vx *= (0.75+0.5*random())
         
-    if y_ball >= height_window - width_ball: # Si la balle touche le bas de l'écran sans toucher la barre
+    if y_ball >= 620:
         vy = 0
-        vx = 0 # Pour stopper la balle
-        vel = 0 # Pour stopper la barre
-        game_over = True # Pour lancer la fonction is_game_over
+        vx = 0
+        vel = 0
+        game_over = True
         
         
-    if x_ball <= 0 or x_ball >= width_window - width_ball: ## Pour detecter les si il y a une collision avec la barre, verifie si la balle est à la hauteur de la barre, et si elle est dans la barre(gauche = left, droite = left + width de la barre) Sussy baka, je ne suis plus sur de cette partie par contre
+    if x_ball <= 0 or x_ball >= 960 - width_ball:
         vx = -vx * 1.05
         
-    if y_ball >= height_window - width_ball - height and x_ball > x_barre and x_ball < x_barre + width:  
+    if y_ball >= 608 - height and x_ball > x_barre and x_ball < x_barre + width:
+        vy = -vy * 1.05
         vx *= (0.75+0.5*random())
         cpt += 1
         window.blit(image_texte, (10, 10))
