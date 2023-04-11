da94c585831d19eab8c1a72a414ca235458e9b6d
Marchenco <106735613+Marchenco@users.noreply.github.com>
2023-04-11 10:24:54 +0200

    Update Title_screen.py

--- Title_screen.py
+++ Title_screen.py
@@ -1,7 +1,11 @@
 import pygame
 from pygame.locals import *
 
+# en attente de finition 
+# bouton fonctionel
+
 WHITE = (255,255,255)
+GREEN = (0,255,0)
 LACK = (0,0,0)
 SIZE = (600, 400)
 color = pygame.Color('lightskyblue3')
@@ -10,6 +14,7 @@ screen.fill(WHITE)
 lock = True
 
 input_rect = pygame.Rect(200, 200, 140, 32)
+pygame.draw.rect(screen,GREEN,input_rect)
 
 while lock :
     for event in pygame.event.get() :
@@ -17,19 +22,16 @@ while lock :
             lock = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if input_rect.collidepoint(event.pos):
-                
+            mouse_pos = pygame.mouse.get_pos()
+            if input_rect.collidepoint(mouse_pos):
+                pass
                 pygame.draw.rect(screen, LACK, input_rect)
                 
             else:
                 active = False
     pygame.display.update()
-
-
-# draw rectangle and argument passed which should
-    # be on screen
-    pygame.draw.rect(screen, LACK, input_rect)
   
+pygame.quit()
 
 
 
