3a1afa5f2f81641b7c4393c451ebba97521b067a
Marchenco <106735613+Marchenco@users.noreply.github.com>
2023-04-20 17:45:36 +0200

    Update Title_screen.py

--- Title_screen.py
+++ Title_screen.py
@@ -11,12 +11,36 @@ color = pygame.Color('lightskyblue3')
 screen = pygame.display.set_mode(SIZE)
 screen.fill(WHITE)
 lock = True
+# finir le délire des classes et regarder la doc python 
+class objet_png_avec_une_hitbox():
+    
+    def __init__(self,image_name,coordonne):
+        
+        self.image_surface = pygame.image.load(image_name).convert() # objet du type surface avec l'image du png
+        
+        self.coordonne = (coordonne[0], coordonne[1]) #les coord x et y pour l'hitbox et l'image(l'objet surface)
+        
+        self.image_pos = screen.blit(self.image_surface, (self.coordonne)) # position de l'image 
+        
+        self.hitbox = pygame.Rect( (self.coordonne), (self.image_surface.get_width(), self.image_surface.get_height())) # objet du type Rect
+        # pygame.display.flip() pour aficher l'image
+        
+    def is_cliked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        x,y = mouse_pos
+        if self.hitbox.collidepoint(mouse_pos):
+            return True
+        return False
+    
+start_button = objet_png_avec_une_hitbox("startbutton.png",(200,75))
 
-start_buttoon_hitbox = pygame.Rect(200, 75, 200, 50)
+#start_buttoon_hitbox = 
 
 #create a surface object, image is drawn on it.
-imp = pygame.image.load("U:\\Documents\\NSI_ex\\TerminalNSI\\startbutton.png").convert()
-screen.blit(imp, (200, 75))
+#imp = pygame.image.load("startbutton.png").convert()
+#screen.blit(imp, (200, 75)) # position de l'image
+
+
 pygame.display.flip()
 
 #pygame.draw.rect(screen,GREEN,start_buttoon_hitbox)
@@ -29,15 +53,16 @@ while lock :
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             print(mouse_pos)
-            x,y = mouse_pos
-            #pygame.draw.rect(screen,GREEN,(x,y,100,40))
-            
-            if start_buttoon_hitbox.collidepoint(mouse_pos):
-                pass
-                pygame.draw.rect(screen, LACK, start_buttoon_hitbox)
+            if start_button.is_cliked():
                 
-            else:
-                active = False
+                
+                # METTRE CODE DU JEU ICI
+                
+                
+                
+                
+                
+                pygame.draw.rect(screen, LACK, pygame.Rect(200, 75, 200, 50))
     pygame.display.update()
   
 pygame.quit()
